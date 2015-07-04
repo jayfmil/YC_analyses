af5c540e9570bb1f1fd40c3e6d226837b04e5de9
jayfmil <jay.f.mil@gmail.com>
2015-07-04 09:51:03 -0400

    work on YC1_regressDifficultyAndTrial.m

--- Multivariate/YC1_regressDifficultyAndTrial.m
+++ Multivariate/YC1_regressDifficultyAndTrial.m
@@ -172,3 +172,24 @@ for e = 1:nElecs
     end
 end
 end
+
+function events = addErrorField(events)
+% add testError field
+% add inner field (1 = inner region, 0 = outer region)
+
+testInd = strcmp({events.type},'NAV_TEST');
+recEvents = events(testInd);
+[events.testError] = deal(NaN);
+[events.recalled] = deal(NaN);
+[events.inner] = deal(NaN);
+sessVec = [events.session];
+trialVec = [events.blocknum];
+for rec = 1:length(recEvents);
+  session = recEvents(rec).session;  
+  trial = recEvents(rec).blocknum;
+  err = recEvents(rec).respPerformanceFactor;
+  ind = sessVec == session & trialVec == trial;
+  [events(ind).testError] = deal(err);
+  [events(ind).inner] = deal(abs(recEvents(rec).objLocs(1)) < 568/30 && abs(recEvents(rec).objLocs(2)) < 7);
+end
+end
