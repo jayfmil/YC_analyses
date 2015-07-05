dab3f80c3823467a4e2dabcdd0976434a2c83815
jayfmil <jay.f.mil@gmail.com>
2015-07-05 12:18:38 -0400

    back to YC1_subject_summary residual support

--- YC1_subject_summary.m
+++ YC1_subject_summary.m
@@ -438,6 +438,7 @@ if ~exist(fullfile(subjPath,fname),'file')
     error('Residuals file %s not found.\n',fname)
 else
     elecData = load(fullfile(subjPath,fname));
+    pow = permute(elecData,[3 2 1]);
     if size(elecData.resid,1) ~= sum(eventsToUse)
         keyboard
     end
