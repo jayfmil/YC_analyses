9aa296ca821000fcf92250112ddcd60cdc858430
jayfmil <jay.f.mil@gmail.com>
2015-07-09 17:14:20 -0400

    streamlining

--- Multivariate/YC1_runMulti.m
+++ Multivariate/YC1_runMulti.m
@@ -11,7 +11,7 @@ end
 % save directory
 f = @(x,y) y{double(x)+1};
 y = {'OrigPower','CorrectedPower'};
-saveDir = fullfile(params.baseBath,f(params.useCorrectedPower,y));
+saveDir = fullfile(params.basePath,f(params.useCorrectedPower,y));
 if ~exist(saveDir,'dir')
     mkdir(saveDir);
 end
