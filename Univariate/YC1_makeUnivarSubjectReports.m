f5688b50ad389e2672546cd90e50cebd853eace5
jayfmil <jay.f.mil@gmail.com>
2015-10-09 14:18:55 -0700

    work on univariate stats

--- Univariate/YC1_makeUnivarSubjectReports.m
+++ Univariate/YC1_makeUnivarSubjectReports.m
@@ -598,39 +598,16 @@ fprintf(fid,'\\end{document}\n\n\n');
 
 
 
-function sout = mergestruct(varargin)
-%MERGESTRUCT Merge structures with unique fields.
-%   Copyright 2009 The MathWorks, Inc.
+function sout = mergestruct(struct1,struct2)
 
-% Start with collecting fieldnames, checking implicitly
-% that inputs are structures.
-fn = [];
-for k = 1:nargin
-    try
-        fn = [fn ; fieldnames(varargin{k})];
-    catch MEstruct
-        throw(MEstruct)
+fields1 = fieldnames(struct1);
+fields2 = fieldnames(struct2);
+
+if isequal(fields1,fields2)
+    sout = cell2struct(fields1,fields1,1);
+    for f = 1:length(fields1)
+        sout.(fields1(f)) = horzcat(struct1.(fields1(f)),struct2.(fields1(f)));       
     end
 end
-
-% Make sure the field names are unique.
-if length(fn) ~= length(unique(fn))
-    error('mergestruct:FieldsNotUnique',...
-        'Field names must be unique');
-end
-
-% Now concatenate the data from each struct.  Can't use
-% structfun since input structs may not be scalar.
-c = [];
-for k = 1:nargin
-    try
-        c = [c ; struct2cell(varargin{k})];
-    catch MEdata
-        throw(MEdata);
-    end
-end
-% Construct the output.
-sout = cell2struct(c, fn, 1);
-
 
 
