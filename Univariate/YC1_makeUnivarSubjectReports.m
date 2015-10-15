bb0b42da81b852484446023547d8e0210a1fec00
jayfmil <jay.f.mil@gmail.com>
2015-10-15 12:26:20 -0700

    some cleanup on univariate code

--- Univariate/YC1_makeUnivarSubjectReports.m
+++ Univariate/YC1_makeUnivarSubjectReports.m
@@ -1,5 +1,5 @@
-function YC1_makeUnivarSubjectReports(subjs,params,overwrite)
-% function YC1_makeUnivarSubjectReports(subjs,params,overwrite)
+function YC1_makeUnivarSubjectReports(subjs,params)
+% function YC1_makeUnivarSubjectReports(subjs,params)
 %
 % Inputs:
 %
@@ -17,10 +17,6 @@ if ~exist('params','var') || isempty(params)
     params = univarParams();
 end
 
-% overwrite exisiting figures?
-if ~exist('overwrite','var') || isempty(overwrite)
-    overwrite = false;
-end
 
 % save directory
 f = @(x,y) y{double(x)+1};
@@ -43,8 +39,6 @@ figDir = fullfile(saveDir,'figs');
 if ~exist('figDir','dir')
     mkdir(figDir)
 end
-
-% store group information
 
 % This loop loads the data from each subject and concatenates all subjects
 % in to one large structure
@@ -99,22 +93,16 @@ for f = 1:length(fields)
     [~,pR(f)] = ttest(-subjDataAll.(fields{f}).r);
     semR(f)   = nanstd(subjDataAll.(fields{f}).r)/sqrt(length(subjDataAll.(fields{f}).r)-1);
     
-
+    % center vertical region
     meanRinner(f)  = nanmean(-subjDataAll.(fields{f}).rInner);
     semRinner(f)   = nanstd(subjDataAll.(fields{f}).rInner)/sqrt(length(subjDataAll.(fields{f}).rInner)-1);
 
+    % outer vertical regions
     meanRouter(f)  = nanmean(-subjDataAll.(fields{f}).rOuter);
     semRouter(f)   = nanstd(subjDataAll.(fields{f}).rOuter)/sqrt(length(subjDataAll.(fields{f}).rOuter)-1);
-
-   % meanROuterInner = nanmean(-subjDataAll.(fields{f}).rOuterInner,3);
-   % semROuterInner = nanstd(subjDataAll.(fields{f}).rOuterInner,0,3)/sqrt(size(subjDataAll.(fields{f}).rOuterInner,3)-1);
-    
-   % meanRinner(f) = meanROuterInner(2);
-   % semRinner(f) = semROuterInner(2);
-    
-   % meanRouter(f) = meanROuterInner(1);
-   % semRouter(f) = semROuterInner(1);    
 end
+
+% Figure 1: average correlation for each freq band for all trials
 figure(1)
 clf
 bar(find(meanR>0),meanR(meanR>0),'linewidth',4,'FaceColor',[150 23 31]/255);
@@ -132,7 +120,7 @@ set(gca,'xticklabel',{'0-3','3-9','40-70','70-200'})
 fname = fullfile(figDir,'corrByFreq.eps');
 print('-depsc2','-loose',fname)
 
-
+% Figure 2: average correlation for each freq band for center trials
 figure(2)
 clf
 bar(find(meanRinner>0),meanRinner(meanRinner>0),'linewidth',4,'FaceColor',[150 23 31]/255);
@@ -143,11 +131,13 @@ set(gca,'xtick',1:f)
 set(gca,'xticklabel',fields')
 grid on
 set(gca,'gridlinestyle',':')
-ylabel('Mean Pearson Coef.','fontsize',24);
-set(gca,'fontsize',24)
+ylabel('Mean Pearson Coef.','fontsize',26);
+set(gca,'fontsize',26)
+set(gca,'xlim',[0 length(meanR)+1])
 fname = fullfile(figDir,'corrByFreqInner.eps');
 print('-depsc2','-loose',fname)
 
+% Figure 3: average correlation for each freq band for outer trials
 figure(3)
 clf
 bar(find(meanRouter>0),meanRouter(meanRouter>0),'linewidth',3,'FaceColor',[150 23 31]/255);
@@ -158,200 +148,11 @@ set(gca,'xtick',1:f)
 set(gca,'xticklabel',fields')
 grid on
 set(gca,'gridlinestyle',':')
-ylabel('Mean Pearson Coef.','fontsize',20);
-set(gca,'fontsize',20)
-
+ylabel('Mean Pearson Coef.','fontsize',26);
+set(gca,'fontsize',26)
+set(gca,'xlim',[0 length(meanR)+1])
 fname = fullfile(figDir,'corrByFreqOuter.eps');
 print('-depsc2','-loose',fname)
-return
-% fname = fullfile(figDir,'corrByFreq.eps');
-% print('-depsc2','-loose',fname)
-
-keyboard
-return 
-
-good = ~cellfun('isempty',{figs.subj});
-figs = figs(good);
-texName = 'lassoChance_report.tex';
-write_texfile(saveDir,texName,figs)
-
-
-curr_dir = pwd;
-cd(saveDir);
-fprintf('Compiling pdf...\n');
-unix(['pdflatex -shell-escape ' fullfile(saveDir, texName)]);
-unix(['rm ' texName(1:end-3) 'aux']);
-unix(['rm ' texName(1:end-3) 'log']);
-fprintf('Done!\n');
-cd(curr_dir);
-
-% compile group report
-texName = 'group_lassoChance_report.tex';
-write_texfile_group(saveDir,texName,figs_group)
-
-curr_dir = pwd;
-cd(saveDir);
-fprintf('Compiling pdf...\n');
-unix(['pdflatex -shell-escape ' fullfile(saveDir, texName)]);
-unix(['rm ' texName(1:end-3) 'aux']);
-unix(['rm ' texName(1:end-3) 'log']);
-fprintf('Done!\n');
-cd(curr_dir);
-
-
-
-
-% Start making the tex file
-function write_texfile(saveDir,texName, figs)
-
-% Write the document. If you do not have write permission, this will crash.
-fid = fopen(fullfile(saveDir,texName),'w');
-
-if fid==-1;
-    error(sprintf('cannot open %s',texName))
-end
-
-% Write out the preamble to the tex doc. This is standard stuff and doesn't
-% need to be changed
-fprintf(fid,'\\documentclass[a4paper]{article} \n');
-fprintf(fid,'\\usepackage[usenames,dvipsnames,svgnames,table]{xcolor}\n');
-fprintf(fid,'\\usepackage{graphicx,multirow} \n');
-fprintf(fid,'\\usepackage{epstopdf} \n');
-fprintf(fid,'\\usepackage[small,bf,it]{caption}\n');
-fprintf(fid,'\\usepackage{subfig,amsmath} \n');
-fprintf(fid,'\\usepackage{wrapfig} \n');
-fprintf(fid,'\\usepackage{longtable} \n');
-fprintf(fid,'\\usepackage{pdfpages}\n');
-fprintf(fid,'\\usepackage{mathtools}\n');
-fprintf(fid,'\\usepackage{array}\n');
-fprintf(fid,'\\usepackage{enumitem}\n');
-fprintf(fid,'\\usepackage{sidecap} \\usepackage{soul}\n');
-
-% fprintf(fid,'\\setlength\\belowcaptionskip{5pt}\n');
-fprintf(fid,'\n');
-fprintf(fid,'\\addtolength{\\oddsidemargin}{-.875in} \n');
-fprintf(fid,'\\addtolength{\\evensidemargin}{-.875in} \n');
-fprintf(fid,'\\addtolength{\\textwidth}{1.75in} \n');
-fprintf(fid,'\\addtolength{\\topmargin}{-.75in} \n');
-fprintf(fid,'\\addtolength{\\textheight}{1.75in} \n');
-fprintf(fid,'\n');
-fprintf(fid,'\\newcolumntype{C}[1]{>{\\centering\\let\\newline\\\\\\arraybackslash\\hspace{0pt}}m{#1}} \n');
-
-fprintf(fid,'\\usepackage{fancyhdr}\n');
-fprintf(fid,'\\pagestyle{fancy}\n');
-fprintf(fid,'\\fancyhf{}\n');
-% fprintf(fid,'\\lhead{Report: %s }\n',strrep(subj,'_','\_'));
-fprintf(fid,'\\rhead{Date created: %s}\n',date);
-
-fprintf(fid,'\\usepackage{hyperref}\n');
-
-% Start the document
-fprintf(fid,'\\begin{document}\n\n\n');
-
-% fprintf(fid,'\\hypertarget{%s}{}\n',region{1});
-
-% This section writes the figures
-for s = 1:length(figs)
-    
-    fprintf(fid,'\\begin{figure}[!h]\n');
-    fprintf(fid,'\\centering\n');    
-    fprintf(fid,'\\includegraphics[width=0.4\\textwidth]{%s}\n',figs(s).perf);
-    fprintf(fid,'\\includegraphics[width=0.4\\textwidth]{%s}\n',figs(s).AUC);
-    fprintf(fid,'\\includegraphics[width=0.4\\textwidth]{%s}\n',figs(s).quarts);
-    fprintf(fid,'\\includegraphics[width=0.4\\textwidth]{%s}\n',figs(s).behavior);    
-    fprintf(fid,'\\caption{%s: region: %s, %d electrodes}\n\n',strrep(figs(s).subj,'_',' '),figs(s).region,figs(s).nElecs);
-    fprintf(fid,'\\end{figure}\n\n\n');
-    if mod(s,2) == 0
-        fprintf(fid,'\\clearpage\n\n\n');
-    end
-end
-
-fprintf(fid,'\\end{document}\n\n\n');
-
-% Start making the tex file
-function write_texfile_group(saveDir,texName, figs)
-
-% Write the document. If you do not have write permission, this will crash.
-fid = fopen(fullfile(saveDir,texName),'w');
-
-if fid==-1;
-    error(sprintf('cannot open %s',texName))
-end
-
-% Write out the preamble to the tex doc. This is standard stuff and doesn't
-% need to be changed
-fprintf(fid,'\\documentclass[a4paper]{article} \n');
-fprintf(fid,'\\usepackage[usenames,dvipsnames,svgnames,table]{xcolor}\n');
-fprintf(fid,'\\usepackage{graphicx,multirow} \n');
-fprintf(fid,'\\usepackage{epstopdf} \n');
-fprintf(fid,'\\usepackage[small,bf,it]{caption}\n');
-fprintf(fid,'\\usepackage{subfig,amsmath} \n');
-fprintf(fid,'\\usepackage{wrapfig} \n');
-fprintf(fid,'\\usepackage{longtable} \n');
-fprintf(fid,'\\usepackage{pdfpages}\n');
-fprintf(fid,'\\usepackage{mathtools}\n');
-fprintf(fid,'\\usepackage{array}\n');
-fprintf(fid,'\\usepackage{enumitem}\n');
-fprintf(fid,'\\usepackage{sidecap} \\usepackage{soul}\n');
-
-% fprintf(fid,'\\setlength\\belowcaptionskip{5pt}\n');
-fprintf(fid,'\n');
-fprintf(fid,'\\addtolength{\\oddsidemargin}{-.875in} \n');
-fprintf(fid,'\\addtolength{\\evensidemargin}{-.875in} \n');
-fprintf(fid,'\\addtolength{\\textwidth}{1.75in} \n');
-fprintf(fid,'\\addtolength{\\topmargin}{-.75in} \n');
-fprintf(fid,'\\addtolength{\\textheight}{1.75in} \n');
-fprintf(fid,'\n');
-fprintf(fid,'\\newcolumntype{C}[1]{>{\\centering\\let\\newline\\\\\\arraybackslash\\hspace{0pt}}m{#1}} \n');
-
-fprintf(fid,'\\usepackage{fancyhdr}\n');
-fprintf(fid,'\\pagestyle{fancy}\n');
-fprintf(fid,'\\fancyhf{}\n');
-% fprintf(fid,'\\lhead{Report: %s }\n',strrep(subj,'_','\_'));
-fprintf(fid,'\\rhead{YC1 Group Report Date created: %s}\n',date);
-
-fprintf(fid,'\\usepackage{hyperref}\n');
-
-% Start the document
-fprintf(fid,'\\begin{document}\n\n\n');
-
-% fprintf(fid,'\\hypertarget{%s}{}\n',region{1});
-
-fprintf(fid,'\\begin{figure}[!h]\n');
-fprintf(fid,'\\centering\n');
-for f = 1:size(figs.N,1)       
-    fprintf(fid,'\\includegraphics[width=0.4\\textwidth]{%s}\n',figs.quarts{f});
-end
-fprintf(fid,'\\caption{%d Subjects: Subject average quartile by time bin.}\n\n',figs.N(1,1));
-fprintf(fid,'\\end{figure}\n\n\n');
-fprintf(fid,'\\clearpage\n\n\n');
-
-fprintf(fid,'\\begin{figure}[!h]\n');
-fprintf(fid,'\\centering\n');
-for f = 1:size(figs.N,1)       
-    fprintf(fid,'\\includegraphics[width=0.4\\textwidth]{%s}\n',figs.acc_hist{f});
-end
-fprintf(fid,'\\caption{%d Subjects: Subject accuracy histogram by time bin.}\n\n',figs.N(1,1));
-fprintf(fid,'\\end{figure}\n\n\n');
-fprintf(fid,'\\clearpage\n\n\n');
-
-fprintf(fid,'\\begin{figure}[!h]\n');
-fprintf(fid,'\\centering\n');
-for f = 1:size(figs.N,1)       
-    fprintf(fid,'\\includegraphics[width=0.4\\textwidth]{%s}\n',figs.auc_hist{f});
-end
-fprintf(fid,'\\caption{%d Subjects: Subject AUC histogram by time bin}\n\n',figs.N(1,1));
-fprintf(fid,'\\end{figure}\n\n\n');
-
-fprintf(fid,'\\begin{figure}[!h]\n');
-fprintf(fid,'\\centering\n');
-
-fprintf(fid,'\\includegraphics[width=0.8\\textwidth]{%s}\n',figs.auc_time_direct);
-fprintf(fid,'\\caption{%d Subjects: Average AUC over time. Dark red: significant after correcting for number of time bins. Light red: $p<.05$.}\n\n',figs.N(1,1));
-fprintf(fid,'\\end{figure}\n\n\n');
-
-fprintf(fid,'\\end{document}\n\n\n');
-
 
 
 function sout = mergestruct(struct1,struct2)
